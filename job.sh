7b5dacb2b84d87ffbb1cbf99b82c669a157d7f47
amirkfir <amirkfir93@gmail.com>
2025-10-07 14:01:24 +0200

    for batch

--- job.sh
+++ job.sh
@@ -2,31 +2,31 @@
 
 ### General options
 ### -- specify queue --
-BSUB -q hpc
+#BSUB -q hpc
 ### -- set the job Name --
-BSUB -J Resnet_TTT
+#BSUB -J Resnet_TTT
 ### -- ask for number of cores (default: 1) --
-#BSUB -n 4
+#BSUB -n 1
 ### -- specify that the cores must be on the same host --
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 4GB of memory per core/slot --
-BSUB -R "rusage[mem=16GB]"
+#BSUB -R "rusage[mem=16GB]"
 ### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot --
-#BSUB -M 5GB
+#BSUB -M 15GB
 ### -- set walltime limit: hh:mm --
-BSUB -W 48:00
+#BSUB -W 48:00
 ### -- set the email address --
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
-#BSUB -u your_email_address
+#BSUB -u amirkfir93@gmail.com
 ### -- send notification at start --
-BSUB -B
+#BSUB -B
 ### -- send notification at completion --
-BSUB -N
+#BSUB -N
 ### -- Specify the output and error file. %J is the job-id --
 ### -- -o and -e mean append, -oo and -eo mean overwrite --
-BSUB -o Output_%J.out
-BSUB -e Output_%J.err
+#BSUB -o Output_%J.out
+#BSUB -e Output_%J.err
 
 source /work3/s242954/ttt_env/bin/activate
 
