55617138dc98329c8f92405ad20e983c9ae59603
amirkfir <amirkfir93@gmail.com>
2025-10-10 11:14:54 +0200

    fix for hpc

--- utils/train_helpers.py
+++ utils/train_helpers.py
@@ -12,6 +12,9 @@ import torch.utils.data
 from utils.rotation import RotateImageFolder
 from models.SSHead import *
 
+import matplotlib
+matplotlib.use('Agg')
+
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 tr_transforms = transforms.Compose([transforms.RandomResizedCrop(224),
 									transforms.RandomHorizontalFlip(),
